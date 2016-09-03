e352fd9f37be51b83bcbc519621ea4fc886a7560
Germán Lena <german.lena@gmail.com>
2016-09-03 15:44:37 -0300

    Finished basic watchface with weather

--- src/c/main.c
+++ src/c/main.c
@@ -2,8 +2,11 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_layer;
 static GFont s_time_font;
+static GFont s_date_font;
 static Layer *s_canvas_layer;
+static TextLayer *s_weather_layer;
 
 static void update_time() {
   // Get a tm structure
@@ -11,40 +14,53 @@ static void update_time() {
   struct tm *tick_time = localtime(&temp);
 
   // Write the current hours and minutes into a buffer
-  static char s_buffer[8];
-  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-                                          "%H:%M" : "%I:%M", tick_time);
+  static char s_time_buffer[8];
+  strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+  
+  static char s_date_buffer[11];
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %e", tick_time);
 
   // Display this time on the TextLayer
-  text_layer_set_text(s_time_layer, s_buffer);
+  text_layer_set_text(s_time_layer, s_time_buffer);
+  text_layer_set_text(s_date_layer, s_date_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
+  
+  // Get weather update every 30 minutes
+  if(tick_time->tm_min % 30 == 0) {
+    // Begin dictionary
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+  
+    // Add a key-value pair
+    dict_write_uint8(iter, 0, 0);
+  
+    // Send the message!
+    app_message_outbox_send();
+  }
 }
 
 static void canvas_update_proc(Layer *layer, GContext *ctx) {
 
   GRect bounds = layer_get_bounds(layer);
   
-  // Set the line color
-  graphics_context_set_stroke_color(ctx, GColorWhite);
-  
-  // Set the fill color
-  graphics_context_set_fill_color(ctx, GColorWhite);
-    
-    
-  // Set the stroke width (must be an odd integer value)
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_context_set_stroke_width(ctx, 1);
   
-  GPoint start_1 = GPoint(25, PBL_IF_ROUND_ELSE(67, 61));
-  GPoint end_1 = GPoint(bounds.size.w - 25, PBL_IF_ROUND_ELSE(67, 61));
+  int first_line_top = PBL_IF_ROUND_ELSE(67, 61);
+  int time_height = 43;
+  int side_margin = 25;
+  
+  GPoint start_1 = GPoint(side_margin, first_line_top);
+  GPoint end_1 = GPoint(bounds.size.w - side_margin - 2, first_line_top);
+  
+  GPoint start_2 = GPoint(side_margin, first_line_top + time_height);
+  GPoint end_2 = GPoint(bounds.size.w - side_margin - 2, first_line_top + time_height);
   
   graphics_draw_line(ctx, start_1, end_1);
-  
-  GPoint start_2 = GPoint(25, PBL_IF_ROUND_ELSE(124, 118));
-  GPoint end_2 = GPoint(bounds.size.w - 25, PBL_IF_ROUND_ELSE(124, 118));
-  
   graphics_draw_line(ctx, start_2, end_2);
 
 }
@@ -56,48 +72,100 @@ static void main_window_load(Window *window) {
 
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(70, 64), bounds.size.w, 50));
+  s_date_layer = text_layer_create(GRect(0, 10, bounds.size.w, 20));
   
   // Create canvas layer
   s_canvas_layer = layer_create(bounds);
   
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_REPET_42));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DOT_FONT_32));
+  s_date_font = fonts_load_custom_font(fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
-  //   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_date_layer, s_date_font);
 
   // Improve the layout to be more like a watchface
-  text_layer_set_background_color(s_time_layer, GColorBlack);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_background_color(s_time_layer, GColorWhite);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
+  
+  text_layer_set_background_color(s_date_layer, GColorWhite);
+  text_layer_set_text_color(s_date_layer, GColorBlack);
+  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+  
+  // Weather
+  
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(0, bounds.size.h - 30, bounds.size.w, 20));
+  
+  // Style the text
+  text_layer_set_background_color(s_weather_layer, GColorWhite);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_weather_layer, "Loading...");
+  text_layer_set_font(s_weather_layer, s_date_font);
+  
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-  
-  // Assign the custom drawing procedure
-  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
-  
-  // Add to Window
+  layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_weather_layer));
   layer_add_child(window_get_root_layer(window), s_canvas_layer);
+  
+  layer_set_update_proc(s_canvas_layer, canvas_update_proc);
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_layer);
+  text_layer_destroy(s_weather_layer);
   
   // Destroy CanvasLayer
   layer_destroy(s_canvas_layer);
   
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  static char temperature_buffer[8];
+  static char conditions_buffer[32];
+  static char weather_layer_buffer[32];
+  
+  // Read tuples for data
+  Tuple *temp_tuple = dict_find(iterator, MESSAGE_KEY_TEMPERATURE);
+  Tuple *conditions_tuple = dict_find(iterator, MESSAGE_KEY_CONDITIONS);
+  
+  // If all data is available, use it
+  if(temp_tuple && conditions_tuple) {
+    snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
+    snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
+  }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
   
-  window_set_background_color(s_main_window, GColorBlack);
+  window_set_background_color(s_main_window, GColorWhite);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -111,6 +179,16 @@ static void init() {
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   update_time();
+  
+  app_message_register_inbox_received(inbox_received_callback);
+  app_message_register_inbox_dropped(inbox_dropped_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  
+  // Open AppMessage
+  const int inbox_size = 128;
+  const int outbox_size = 128;
+  app_message_open(inbox_size, outbox_size);
 }
 
 static void deinit() {
